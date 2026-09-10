231f4f82f2d1da60804de359bc821c66e0d5349f
cauacouto <cauacouto022@gmail.com>
2026-09-10 00:08:20 -0300

    Add EventPublish class for publishing account creation events to Kafka

---
+++ account-service/src/main/java/com/example/account/service/producer/EventPublish.java
@@ -0,0 +1,30 @@
+package com.example.account.service.producer;
+
+import com.example.account.service.domin.Conta;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EventPublish {
+
+    private final KafkaTemplate<String,Object> kafkaTemplate;
+
+    public EventPublish(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void publishEvent(Conta conta){
+
+        AccontCreatEvent event = new AccontCreatEvent(
+                conta.getUuid(),
+                conta.getTitular(),
+                conta.getCpf(),
+                conta.getDataNascimento(),
+                conta.getTipoConta()
+        );
+        kafkaTemplate.send("accont.creates",event.uuid().toString(),event);
+        log.info("evento publicado | accountID={}", event.uuid());
+    }
+}
