9f2b98518683a06b94353579de8e242a5164b119
cauacouto <cauacouto022@gmail.com>
2026-09-11 00:30:56 -0300

    Refactor Conta class: rename uuid to id, update constructor, and add missing getters/setters

--- account-service/src/main/java/com/example/account/service/domin/Conta.java
+++ account-service/src/main/java/com/example/account/service/domin/Conta.java
@@ -2,7 +2,6 @@ package com.example.account.service.domin;
 
 import com.example.account.service.Enums.TipoConta;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,16 +10,13 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
-@Table
-@Getter
-@Setter
+@Table(name = "CONTA_DB")
 
 public class Conta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-
-    private UUID uuid;
+    private UUID id;
     private String titular;
     private String cpf;
     private BigDecimal saldo;
@@ -28,15 +24,63 @@ public class Conta {
     @Enumerated(EnumType.STRING)
     private TipoConta tipoConta;
 
-    public Conta(UUID uuid, String titular, String cpf, LocalDate dataNascimento, TipoConta tipoConta) {
-        this.uuid = uuid;
+    public Conta() {
+
+    }
+
+    public Conta(UUID id, String titular, String cpf, LocalDate dataNascimento, TipoConta tipoConta) {
+        this.id = id;
         this.titular = titular;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
         this.tipoConta = tipoConta;
     }
 
-    public Conta() {
+    public UUID getId() {
+        return id;
+    }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public TipoConta getTipoConta() {
+        return tipoConta;
+    }
+
+    public void setTipoConta(TipoConta tipoConta) {
+        this.tipoConta = tipoConta;
     }
 }
