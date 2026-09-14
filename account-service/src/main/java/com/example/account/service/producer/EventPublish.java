d904b8a79a35810380d948deb5cb8a53ed3a00be
cauacouto <cauacouto022@gmail.com>
2026-09-13 22:15:28 -0300

    Add TranfereciaProducer method for publishing transfer creation events

--- account-service/src/main/java/com/example/account/service/producer/EventPublish.java
+++ account-service/src/main/java/com/example/account/service/producer/EventPublish.java
@@ -1,6 +1,7 @@
 package com.example.account.service.producer;
 
 import com.example.account.service.domin.Conta;
+import com.example.account.service.domin.Transfer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,21 @@ public class EventPublish {
         );
         kafkaTemplate.send("accont.creates",event.uuid().toString(),event);
         log.info("evento publicado | accountID={}", event.uuid());
+    }
+
+
+    public void TranfereciaProducer(Transfer transfer){
+
+        TranferCreateEventDto eventDto = new TranferCreateEventDto(
+                transfer.getUuid(),
+                transfer.getIdOrigem(),
+                transfer.getIdDestino(),
+                transfer.getValor(),
+                transfer.getFormaPagamento(),
+                transfer.getRealizadaEm()
+        );
+        kafkaTemplate.send("tranfer.created",eventDto.idTranfercia(),eventDto);
+        log.info("transacao publicada | transacaoId={}",eventDto.idTranfercia());
+
     }
 }
