7fc40c40524475dc9de18467c7ab154d8bda86af
cauacouto <cauacouto022@gmail.com>
2026-09-13 21:07:13 -0300

    Rename AccontCreatEvent to AccontCreatEventDto for clarity and consistency

--- account-service/src/main/java/com/example/account/service/producer/AccontCreatEventDto.java
+++ account-service/src/main/java/com/example/account/service/producer/AccontCreatEventDto.java
@@ -5,7 +5,7 @@ import com.example.account.service.Enums.TipoConta;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record AccontCreatEvent(UUID uuid,String titular, String cpf, LocalDate dataNascimento, TipoConta tipoConta
+public record AccontCreatEventDto(UUID uuid, String titular, String cpf, LocalDate dataNascimento, TipoConta tipoConta
 
                                ) {
 }
