4b08b99570362917f8301adab240e89eaf49a8e4
cauacouto <cauacouto022@gmail.com>
2026-09-15 00:04:30 -0300

    Rename ProduceKafkaConfig to ProduceKafkaConfigConta and update producerFactory to use KafkaAvroSerializer for AccountCreatedEvent

--- account-service/src/main/java/com/example/account/service/config/ProduceKafkaConfigConta.java
+++ account-service/src/main/java/com/example/account/service/config/ProduceKafkaConfigConta.java
@@ -1,5 +1,7 @@
 package com.example.account.service.config;
 
+import com.account.service.AccountCreatedEvent;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,37 +10,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class ProduceKafkaConfig {
+public class ProduceKafkaConfigConta {
 
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootsTrapServes;
 
-    @Bean
-    public ProducerFactory<String,Object> producerFactory(){
+    @Value("${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistry;
+
+    @Bean()
+    public ProducerFactory<String, AccountCreatedEvent> producerFactory(){
+
         Map<String,Object> configProps = new HashMap<>();
 
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootsTrapServes);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         //serializar o valor com json automaticamente
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,KafkaAvroSerializer.class);
+        configProps.put("schema.registry.url",schemaRegistry);
 
-        //iclui o tipo java no header da mensagem (ajuda o consumer a desserializar)
-        configProps.put(JacksonJsonSerializer.ADD_TYPE_INFO_HEADERS,false);
+
 
         return new DefaultKafkaProducerFactory<>(configProps);
 
     }
 
-    @Bean
-    public KafkaTemplate<String,Object> kafkaTemplate(){
+    @Bean("accountKafkaTemplate")
+    public KafkaTemplate<String,AccountCreatedEvent> kafkaTemplate(){
         return new KafkaTemplate<>(producerFactory());
     }
 }
